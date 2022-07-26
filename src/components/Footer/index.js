8dabb21991c3282b483c918163919b4aa369ce74
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-07-26 09:33:10 -0500

    basic header and footer components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+
+    return (
+        <footer className='footer'>
+            <h2>Follow Me</h2>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+
+    return (
+        <header className='header'>
+            <h1>mongamonga_</h1>
+        </header>
+    )
+}
+
+export default Header
